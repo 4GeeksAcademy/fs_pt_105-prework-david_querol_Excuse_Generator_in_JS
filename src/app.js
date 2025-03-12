45b6d905662055895c421fed117acfc9dddef511
BOTH-AGENCY <morellans@gmail.com>
2025-03-12 17:26:14 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+document.addEventListener("DOMContentLoaded", function() {
+        // definimos los arregleos con las palabras.
+
+        let item1 = ["Mi perro ", "Una iguana ", "Un alien ", "Elion Musk ", "Un meteorito "];
+        let item2 = ["se comió ", "exterminó ", "atemorizó ", "orinó ", "aspiró "] ;
+        let item3 = ["los apuntes ", "la bicicleta " , "el ordenador " , "la mesa " , "la puerta "] ;
+        let item4 = ["súbitamente.", "esta mañana." , "en el salón de estar." , "de forma inquietante."] ;
+
+        //funcion par seleccionar los items random.
+        let rand1 = Math.floor(Math.random() * item1.length) ;
+        let rand2 = Math.floor(Math.random() * item2.length) ;
+        let rand3 = Math.floor(Math.random() * item3.length) ;
+        let rand4 = Math.floor(Math.random() * item4.length) ;
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+        //escribibos en la etiqueta del id excusa.
+        document.querySelector("#excusa").innerHTML = item1[rand1] + item2[rand2] + item3[rand3] + item4[rand4] ;
+
+
+    }
+)
